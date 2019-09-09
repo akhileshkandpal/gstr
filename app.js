cb4d348d1543085e11430db5ba3836202d042a1c
Akhilesh Kumar Kandpal <akhilesh.kandpal42@gmail.com>
2019-09-09 15:14:44 +0530

    Removed error

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/",(req,res) => {
     res.render("GSTR1_Invoices.ejs");
 })
 
-app.listen(3000);
-//app.listen(process.env.PORT || 5000);
+//app.listen(3000);
+app.listen(process.env.PORT || 5000);
 
