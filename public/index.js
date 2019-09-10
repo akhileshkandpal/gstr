689813a5dac9d3ccb2d90c542427604ecd1fb729
Akhilesh Kumar Kandpal <akhilesh.kandpal42@gmail.com>
2019-09-10 18:50:40 +0530

    Updated B2CL

--- public/index.js
+++ public/index.js
@@ -5,6 +5,9 @@ document.getElementById("btn-submit").addEventListener("click",function(){
     if(gstr.filing_typ == "M"){
         filingPeriod = "month"
     }else{filingPeriod = "quater ended in month"}
+ 
+    //For B2B Transactions   
+    
     document.getElementById("b2bHeading").innerHTML = "GSTR 1/4A B2B Transactions for GSTIN " + gstr.gstin + " holder for the " + filingPeriod + " of " + months[parseInt(gstr.fp.substring(0,2))-1] + "-" + gstr.fp.substring(2,6) + ".";  
     var b2b = gstr["b2b"];
     var b2bArr = [];
@@ -32,6 +35,12 @@ document.getElementById("btn-submit").addEventListener("click",function(){
                 html = html + "<th>" + invoices[j]["rchrg"] + "</th>"
                 html = html + "<th>" + item["txval"] + "</th>"
                 html = html + "<th>" + item["rt"] + "%" + "</th>"
+                if(!isNaN(invoices[j]["diff_percent"])){
+                    html = html + "<th>" + (invoices[j]["diff_percent"]*100) + "%" + "</th>"
+                }else{
+                    html = html + "<th>"+ "</th>"
+                }
+                
 
                 if(item["iamt"] != undefined){
                     html = html + "<th>" + item["iamt"] + "</th>"
@@ -60,8 +69,147 @@ document.getElementById("btn-submit").addEventListener("click",function(){
 
 
     }
-    document.getElementById("tblhead_gstrB2B").innerHTML = "<th>GSTIN</th><th>Invoice No.</th><th>Invoice Date</th><th>POS</th><th>Invoice Value</th><th>Invoice Type</th><th>Reverse Charge</th><th>Taxable Value</th><th>Rate</th><th>IGST</th><th>CGST</th><th>SGST</th>"
+
+
+    document.getElementById("tblhead_gstrB2B").innerHTML = "<th>GSTIN</th><th>Invoice No.</th><th>Invoice Date</th><th>POS</th><th>Invoice Value</th><th>Invoice Type</th><th>Reverse Charge</th><th>Taxable Value</th><th>Rate</th><th>Applicable % of Tax</th><th>IGST</th><th>CGST</th><th>SGST</th>"
     document.getElementById("table1").innerHTML = html;
+    
+    //For B2B Amendment list
+    document.getElementById("b2baHeading").innerHTML = "GSTR 1/4A B2B Amendments for GSTIN " + gstr.gstin + " holder for the " + filingPeriod + " of " + months[parseInt(gstr.fp.substring(0,2))-1] + "-" + gstr.fp.substring(2,6) + ".";
+    var b2ba = gstr["b2ba"];
+    var html=""
+    
+    for(var i = 0; i < b2ba.length; i++){
+        
+        var gstin = b2ba[i]["ctin"];
+        var invoices = b2ba[i]["inv"];
+        for(var j = 0; j < invoices.length; j++){
+            var items = invoices[j]["itms"];
+            
+            for(var k = 0; k < items.length; k++){
+                html = html + "<tr>"
+                // b2bArr.push([gstin,items[k]["itm_det"]["txval"],items[k]["itm_det"]["rt"],items[k]["itm_det"]["csamt"],items[k]["itm_det"]["samt"],items[k]["itm_det"]["camt"]]);
+                var item = items[k]["itm_det"];
+                
+                html = html + "<th>" + gstin + "</th>"
+                html = html + "<th>" + invoices[j]["inum"] + "</th>"
+                html = html + "<th>" + invoices[j]["idt"] + "</th>"
+                html = html + "<th>" + pos[parseInt(invoices[j]["pos"])-1] + "</th>"
+                html = html + "<th>" + invoices[j]["val"] + "</th>"
+                html = html + "<th>" + invoices[j]["inv_typ"] + "</th>"
+                html = html + "<th>" + invoices[j]["rchrg"] + "</th>"
+                html = html + "<th>" + item["txval"] + "</th>"
+                html = html + "<th>" + item["rt"] + "%" + "</th>"
+
+                if(!isNaN(invoices[j]["diff_percent"])){
+                    html = html + "<th>" + (invoices[j]["diff_percent"]*100) + "%" + "</th>"
+                }else{
+                    html = html + "<th>"+ "</th>"
+                }
+
+                if(item["iamt"] != undefined){
+                    html = html + "<th>" + item["iamt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+                if(item["camt"] != undefined){
+                    html = html + "<th>" + item["camt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+                if(item["samt"] != undefined){
+                    html = html + "<th>" + item["samt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+
+
+                html = html + "</tr>"
+                               
+            }    
+            
+            
+        }
+
+
+
+    }
+    document.getElementById("tblhead_gstrB2Ba").innerHTML = "<th>GSTIN</th><th>Invoice No.</th><th>Invoice Date</th><th>POS</th><th>Invoice Value</th><th>Invoice Type</th><th>Reverse Charge</th><th>Taxable Value</th><th>Rate</th><th>Applicable % of Tax</th><th>IGST</th><th>CGST</th><th>SGST</th>"
+    document.getElementById("table3").innerHTML = html;
+
+    //For B2CL list
+    document.getElementById("b2clHeading").innerHTML = "GSTR 1/4A B2CL Invoices for GSTIN " + gstr.gstin + " holder for the " + filingPeriod + " of " + months[parseInt(gstr.fp.substring(0,2))-1] + "-" + gstr.fp.substring(2,6) + ".";
+    var b2cl = gstr["b2cl"];
+    var html=""
+    
+    for(var i = 0; i < b2cl.length; i++){
+        
+        var gstin = b2cl[i]["ctin"];
+        var invoices = b2cl[i]["inv"];
+        for(var j = 0; j < invoices.length; j++){
+            var items = invoices[j]["itms"];
+            
+            for(var k = 0; k < items.length; k++){
+                html = html + "<tr>"
+                // b2bArr.push([gstin,items[k]["itm_det"]["txval"],items[k]["itm_det"]["rt"],items[k]["itm_det"]["csamt"],items[k]["itm_det"]["samt"],items[k]["itm_det"]["camt"]]);
+                var item = items[k]["itm_det"];
+                
+                html = html + "<th>" + gstin + "</th>"
+                html = html + "<th>" + invoices[j]["inum"] + "</th>"
+                html = html + "<th>" + invoices[j]["idt"] + "</th>"
+                html = html + "<th>" + pos[parseInt(invoices[j]["pos"])-1] + "</th>"
+                html = html + "<th>" + invoices[j]["val"] + "</th>"
+                if( invoices[j]["inv_typ"] != undefined){
+                    html = html + "<th>" +  invoices[j]["inv_typ"] + "</th>"
+                }else{
+                	html = html + "<th>" + "</th>"
+                }
+
+                
+                
+                html = html + "<th>" + item["txval"] + "</th>"
+                html = html + "<th>" + item["rt"] + "%" + "</th>"
+
+                if(!isNaN(invoices[j]["diff_percent"])){
+                    html = html + "<th>" + (invoices[j]["diff_percent"]*100) + "%" + "</th>"
+                }else{
+                    html = html + "<th>"+ "</th>"
+                }
+
+                if(item["iamt"] != undefined){
+                    html = html + "<th>" + item["iamt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+                if(item["camt"] != undefined){
+                    html = html + "<th>" + item["camt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+                if(item["samt"] != undefined){
+                    html = html + "<th>" + item["samt"] + "</th>"
+                }else{
+                	html = html + "<th>" + 0 + "</th>"
+                }
+
+
+                html = html + "</tr>"
+                               
+            }    
+            
+            
+        }
+
+
+
+    }
+    document.getElementById("tblhead_gstrb2cl").innerHTML = "<th>GSTIN</th><th>Invoice No.</th><th>Invoice Date</th><th>POS</th><th>Invoice Value</th><th>Invoice Type</th><th>Taxable Value</th><th>Rate</th><th>Applicable % of Tax</th><th>IGST</th><th>CGST</th><th>SGST</th>"
+    document.getElementById("table4").innerHTML = html;
+
+
+
+
+    //For Credit Note Transaction
 
     document.getElementById("cnHeading").innerHTML = "GSTR 1/4A Credit Notes Transactions for GSTIN " + gstr.gstin + " holder for the " + filingPeriod + " of " + months[parseInt(gstr.fp.substring(0,2))-1] + "-" + gstr.fp.substring(2,6) + ".";
     var htmlcn = ""
@@ -87,6 +235,7 @@ document.getElementById("btn-submit").addEventListener("click",function(){
                 htmlcn =  htmlcn + "<th>" + note.val + "</th>"
                 htmlcn =  htmlcn + "<th>" + item.txval + "</th>"     
                 htmlcn =  htmlcn + "<th>" + item.rt + "%" + "</th>"
+                
                 if(item["iamt"] != undefined){
                     htmlcn = htmlcn + "<th>" + item["iamt"] + "</th>"
                 }else{
